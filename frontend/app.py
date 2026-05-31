632e7dbe22e61de485e7b4932abe616d5c74a3ae
toledosofia <stoledo@fi.uba.ar>
2026-05-31 17:33:42 -0300

    configuraciones de login, secret_key y registro de bp

---
+++ frontend/app.py
@@ -0,0 +1,21 @@
+from flask import Flask
+from datetime import timedelta
+from frontend.routes.admin_routes import admin_front_bp
+from frontend.routes.publico_routes import publico_bp
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+app = Flask(__name__)
+
+app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY", "clave-local-desarrollo")
+app.config["PERMANENT_SESSION_LIFETIME"] = timedelta(hours=8)
+app.config["API_BASE_URL"] = os.environ.get("API_BASE_URL")
+app.register_blueprint(admin_front_bp)
+app.register_blueprint(publico_bp)
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True, port=3000)
